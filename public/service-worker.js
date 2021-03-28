f5d1888fcc6e17380cc19343b23fa82afa5b5ba8
KarrynS <karrynsaw@gmail.com>
2021-03-28 14:02:50 +1100

    fixing pathway

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,8 +5,7 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
     "/",
-    "/index.html",
-    "index.js",
+    "/index.js",
     "/styles.css",
     "/db.js",
     "/manifest.webmanifest",
